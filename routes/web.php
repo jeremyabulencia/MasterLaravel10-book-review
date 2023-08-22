2bcf0996b1f9f9f775218f15c4554b8ac4735ed1
Jeremy <jabulencia@yondu.com>
2023-08-22 15:14:38 +0800

    reviews and ratings fix

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
 
 Route::group(['prefix' => '/doc'], function ($route) {
